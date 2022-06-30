08b4c32b352876dac5bb2191fb74a32a40ec0b25
Kashaedov Vitaliy <Vitalickashaedov@mail.ru>
2022-06-30 19:02:28 +0300

    zadacha6

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+
+if  (number1%2 == 0)
+{
+    Console.WriteLine ($"Введенное Вами число {number1} является четным");
+}
+else  
+{
+  Console.WriteLine ($"Введенное Вами число {number1} является нечетным");
+}
